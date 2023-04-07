47b9ff0e57590a577599cf163ac091dfc3aaa25d
hyagossiz <hyagoassiz@hotmail.com>
2023-04-07 11:54:29 -0300

    inserção de banco de dados SQLITE

---
+++ db/conn.js
@@ -0,0 +1,25 @@
+const Sequelize = require("sequelize")
+
+
+// BANCO DE DADOS MYSQL
+// const sequelize = new Sequelize("tasks", "root", "1234", {
+//     host: "localhost",
+//     dialect: "mysql"
+// })
+
+// BANCO DE DADOS SQLITE
+const sequelize = new Sequelize({
+    dialect: "sqlite",
+    storage: "./tasks.sqlite"
+})
+
+try {
+    sequelize.authenticate()
+    console.log("Conectado ao banco de dados")
+} catch (error) {
+    console.log(`Não foi possível conectar ao MySql: ${error}`)
+}
+
+
+
+module.exports = sequelize
